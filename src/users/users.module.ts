abde03b51e759acae1ea7e6606c8ab5da2843c71
Marcelo Araújo Silva <marceloaraujosilva739@gmail.com>
2026-04-28 00:26:44 -0300

    adjusted prisma modules path

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users.controller.js';
 import { ServiceService } from './service/service.service.js';
-import { PrismaModule } from '../prisma.module.js';
+import { PrismaModule } from '../common/prisma/prisma.module.js';
 
 
 @Module({
